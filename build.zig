a9524849bb560d2e6ad4ef93a07385316f95f03c
mgord9518 <mgord9518@gmail.com>
2022-08-28 20:37:11 -0600

    Initial commit

---
+++ build.zig
@@ -0,0 +1,13 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardReleaseOptions();
+
+    const main_tests = b.addTest("src/main.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+}
